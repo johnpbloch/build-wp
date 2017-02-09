ff390a6757361bd4d44d3f6d76680e49185d4e2e
John P Bloch <johnpbloch@gmail.com>
2017-02-09 17:09:14 -0600

    Add some logic to the build script

--- build-wp.sh
+++ build-wp.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+    type="master"
+    ref="trunk"
+else
+    case "$1" in
+        branch)
+            type="branch"
+            ref="branches/$2"
+            ;;
+        tag)
+            type="tag"
+            ref="tags/$2"
+            ;;
+        *)
+            echo "Invalid type"
+            exit 1
+            ;;
+    esac
+fi
 
+if [ -d /tmp/wp ]; then
+    rm -rf /tmp/wp
+fi
+
+svn export --ignore-externals "https://develop.svn.wordpress.org/$ref/" /tmp/wp/
+
+cd /tmp/wp/
+
+npm install
