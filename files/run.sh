38725c6b93694d56cba8c24589b61c887096d6af
John P. Bloch <johnpbloch@users.noreply.github.com>
2020-04-27 10:10:41 -0500

    Add a noop command to stop the build from failing

--- files/run.sh
+++ files/run.sh
@@ -339,8 +339,8 @@ function run(){
         echo "Building trunk..."
         build_trunk && (echo $hash > /tmp/wp-build/branches/master)
       fi
-    elif [ "$branch" == "refs/heads/5.3" ] ; then
-      # Skip bad branch name
+    elif [ "$branch" == "refs/heads/5.3" ]; then
+      : # Skip bad branch name
     elif [ "$hash" != "$(cat branches/$branch)" ]; then
       echo "Building branch $branch..."
       build_branch $branch && (echo $hash > /tmp/wp-build/branches/$branch)
